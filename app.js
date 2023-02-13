04cc1b6b6f5af758e3924cf9af327378a0f042ed
Tavo <tavo@macbook-air-de-lucrecia.home>
2023-02-13 12:19:46 +0100

    Failing eslint test

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
   res.send('ok')
-})
+});
 
 app.get('/version', (req, res) => {
   res.send(version)
-})
+});
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
-})
+});
